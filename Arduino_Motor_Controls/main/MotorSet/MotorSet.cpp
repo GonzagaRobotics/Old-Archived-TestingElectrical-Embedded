348c927a389aa9b44ee3a03698d4b4aabd7dff94
Matthew VonWahlde <101604242+mvonwahlde@users.noreply.github.com>
2022-11-12 13:24:05 -0800

    Adding constructor and destructor

--- Arduino_Motor_Controls/main/MotorSet/MotorSet.cpp
+++ Arduino_Motor_Controls/main/MotorSet/MotorSet.cpp
@@ -1,23 +1,29 @@
-#include "MotorController.h"
 #include "MotorSet.h"
 
 
 MotorSet::MotorSet(){
-
+    numMotors = 0;
 }
 
 MotorSet::~MotorSet(){
+    numMotors = 0;
+}
+
+void MotorSet::addMotor(int pin1, int pin2, int pinEn, char side{
+    if(numMotors < 8){
+        set[numMotors] = new MotorController(pin1, pin2, pinEn, side);
+        numMotors++;
+    }
+}
+
+void MotorSet::driveForwards(){
 
 }
 
-MotorSet::driveForwards(){
+void MotorSet::driveBackwards(){
 
 }
 
-MotorSet::driveBackwards(){
-
-}
-
-MotorSet::stop(){
+void MotorSet::stop(){
     
 }
--- Arduino_Motor_Controls/main/MotorSet/MotorSet.h
+++ Arduino_Motor_Controls/main/MotorSet/MotorSet.h
@@ -1,8 +1,11 @@
-#ifndef MOTOR_CONTROLLER_H
-#define MOTOR_CONTROLLER_H
+#ifndef MOTOR_SET_H
+#define MOTOR_SET_H
 
+#include "MotorController.h"
 #include <Arduino.h>
 
+
+#define maxMotors 8
 
 class MotorSet{
     public:
@@ -12,8 +15,12 @@ class MotorSet{
         void driveForwards(double speed);
         void driveBackwards(double speed);
         void stop();
-    private:
 
+        void addMotor(int pin1, int pin2, int pinEn, char side);
+    private:
+        MotorController* set[maxMotors];
+        numMotors;
+        
 }
 
 #endif
