db78308ac1921bfbf4c9d5c7acfde41126de24f4
Matthew VonWahlde <101604242+mvonwahlde@users.noreply.github.com>
2022-12-04 14:31:20 -0800

    Trying to fix GitHub

--- Arduino_Motor_Controls/main/src/CPPGPIO/CPPGPIO.cpp
+++ Arduino_Motor_Controls/main/src/CPPGPIO/CPPGPIO.cpp
@@ -10,7 +10,13 @@ Description: Function file for ESP32 GPIO in C++
 
 
 void setPinMode(int pin, int direction){
+    // To set as simple output GPIO_FUNCx_OUT_SEL_CFG = 0x100
+    //      First 8 bits are GPIO_FUNCx_OUT_SEL
 
+    // Set as always enable GPIO_FUNCx_OUT_SEL_CFG
+    //      Set the CPIO_FUNCx_OEN_SEL bit 
+    
+    // Set the GPIO_ENABLE_DATAx in GPIO_ENABLE_REG
 }
 
 
