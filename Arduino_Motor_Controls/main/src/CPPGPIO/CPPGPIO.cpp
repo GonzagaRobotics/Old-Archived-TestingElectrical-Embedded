9dd9eef8d770131f0857adc8b469fe2c8f43c821
Matthew VonWahlde <101604242+mvonwahlde@users.noreply.github.com>
2022-12-30 20:04:00 -0800

    First attempt at pin setup and output

--- Arduino_Motor_Controls/main/src/CPPGPIO/CPPGPIO.cpp
+++ Arduino_Motor_Controls/main/src/CPPGPIO/CPPGPIO.cpp
@@ -5,18 +5,61 @@ Date Created: 12/3/22
 Date Modified: 12/3/22
 Description: Function file for ESP32 GPIO in C++
 */
-// 12/30/2022 test
+
 #include "CPPGPIO.h"
 
 
-void setPinMode(int pin, int direction){
+void setOutputPin(int pin, int direction){
+    if(pin < MIN_GPIO_PIN || pin > MAX_GPIO_PIN)
+        return;
+
     // To set as simple output GPIO_FUNCx_OUT_SEL_CFG = 0x100
     //      First 8 bits are GPIO_FUNCx_OUT_SEL
+    volatile unsigned int* pinFuncSelReg = REGISTER_ADDRESS + 0x530 + (4*pin);
+    *pinFuncSelReg |= 0x100;
 
     // Set as always enable GPIO_FUNCx_OUT_SEL_CFG
     //      Set the CPIO_FUNCx_OEN_SEL bit 
+    *pinFuncSelReg |= 0x400;
+
+    // Set the bit the GPIO_ENABLE_DATAx in GPIO_ENABLE(0 or 1)_REG
+    volatile unsigned int* enableDataAddress;
     
-    // Set the GPIO_ENABLE_DATAx in GPIO_ENABLE(0 or 1)_REG
+    if(pin <= 31){
+        enableDataAddress = REGISTER_ADDRESS + 0x20 + power(2,pin);
+        *enableDataAddress |= 0x1;
+    } else {
+        enableDataAddress = REGISTER_ADDRESS + 0x2C + power(2,pin-32);
+        *enableDataAddress |= 0x1;
+    }
+}
+
+
+void digitalOutput(int pin, int value){
+    // For the nth pin, set the nth bit in GPIO_OUT_DATA
+    if(pin < MIN_GPIO_PIN || pin > MAX_GPIO_PIN)
+        return;
+
+    volatile unsigned int* outputDataAddress;
+    
+    if(pin <= 31){
+        outputDataAddress = REGISTER_ADDRESS + 0x04;
+        if(value == 0)
+            *outputDataAddress &= (power(2,32) - 1) - power(2,pin);
+        else{
+            outputDataAddress += power(2,pin);
+            *outputDataAddress |= 0x1;
+        }
+        
+    } else {
+        outputDataAddress = REGISTER_ADDRESS + 0x10;
+        if(value == 0)
+            *outputDataAddress &= (power(2,8) - 1) - power(2,pin-32);
+        else{
+            outputDataAddress += power(2,pin-32);
+            *outputDataAddress |= 0x1;
+        }
+    }
 }
 
 
@@ -30,11 +73,17 @@ void attachPWMChannel(int pin, int channel){
 }
 
 
-void digitalOutput(int pin, int value){
-    // For the nth pin, set the nth bit in GPIO_OUT_DATA
-}
-
-
 void writePWMChannel(int pin, int dutyCycle){
 
+}
+
+int power(int base, int exp){
+    // only positive exponents
+    int value = 1;
+
+    if(exp > 0)
+        for(int i = 0; i < exp; i++)
+            value *= base;
+        
+    return value;
 }
--- Arduino_Motor_Controls/main/src/CPPGPIO/CPPGPIO.h
+++ Arduino_Motor_Controls/main/src/CPPGPIO/CPPGPIO.h
@@ -9,8 +9,12 @@ Description: Header file for ESP32 GPIO in C++
 #ifndef CPPGPIO_H
 #define CPPGPIO_H
 
-#define somePointer (*((volatile uint8_t *) 0x00))
+#define MAX_GPIO_PIN 39
+#define MIN_GPIO_PIN 0
 
+#define somePointer (*((volatile unsigned int *) 0x00))
+
+#define REGISTER_ADDRESS ((volatile unsigned int *)0x3FF44000)
 
 void setPinMode(int, int);
 void setUpPWMChannel(int, int, int);
@@ -18,5 +22,6 @@ void attachPWMChannel(int, int);
 
 void digitalOutput(int, int);
 void writePWMChannel(int, int);
+int power(int, int);
 
 #endif
